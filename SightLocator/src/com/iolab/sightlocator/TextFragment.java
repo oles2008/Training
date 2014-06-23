69cb449689ff8cb8a5d4bfbf7698d49c4c83e959
oles2008 <oles2008@i.ua>
2014-06-23 19:39:59 +0300

    Logs removed

--- SightLocator/src/com/iolab/sightlocator/TextFragment.java
+++ SightLocator/src/com/iolab/sightlocator/TextFragment.java
@@ -10,13 +10,11 @@ import android.view.ViewGroup;
 public class TextFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
-		Log.d("MyLogs","onCreate started");
 		super.onCreate(savedInstanceState);
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-		Log.d("MyLogs","onCreateView started");
 		return inflater.inflate(R.layout.text_fragment, container, false);
 	}
 }
