6e4f03d1d9624b14bc35d155016f4a7aa2458d9a
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2014-12-02 19:40:59 +0200

    refactored gMap to a field; formatted some bits of code for better looking

--- SightLocator/src/com/iolab/sightlocator/SightsMapFragment.java
+++ SightLocator/src/com/iolab/sightlocator/SightsMapFragment.java
@@ -1,26 +1,24 @@
 package com.iolab.sightlocator;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnAttachStateChangeListener;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class SightsMapFragment extends Fragment {
+	private GoogleMap gMap;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,32 +27,42 @@ public class SightsMapFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View sightsmapFragment = inflater.inflate(R.layout.map_fragment,
-				container, false);
+		View sightsmapFragment = inflater.inflate(
+				R.layout.map_fragment,
+				container,
+				false);
 
 		return sightsmapFragment;
 	}
 	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
+
 		super.onActivityCreated(savedInstanceState);
 
 		LocationManager locationManager = (LocationManager) getActivity()
 				.getSystemService(Context.LOCATION_SERVICE);
 		// locationManager.req(LocationManager.NETWORK_PROVIDER, 0,0,)
+		
 		Location initialLocation = locationManager
 				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		
 		if (initialLocation == null) {
 			initialLocation = locationManager
 					.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 		}
-		GoogleMap gMap = ((MapFragment) getFragmentManager().findFragmentById(
-				R.id.map)).getMap();
+		
+		gMap = ((MapFragment) getFragmentManager()
+				.findFragmentById(R.id.map))
+				.getMap();
+		
 		if ((gMap != null) && (initialLocation != null)) {
-			LatLng initialCoord = new LatLng(initialLocation.getLatitude(),
+			LatLng initialCoord = new LatLng(
+					initialLocation.getLatitude(),
 					initialLocation.getLongitude());
 
 			gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialCoord, 15));
+			
 			gMap.addMarker(new MarkerOptions()
 					.position(initialCoord)
 					.title("You are here")
@@ -62,8 +70,8 @@ public class SightsMapFragment extends Fragment {
 							.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
 			
 			gMap.addMarker(new MarkerOptions()
-		    .position(new LatLng(49.8367019,24.0048451))
-		    .title("Hello world"));
+		    		.position(new LatLng(49.8367019,24.0048451))
+		    		.title("Hello world"));
 
 		}
 	}
