2ccbd7c260a6f885545a8b3f423959ce33e357b3
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2014-12-02 20:03:24 +0200

    formatted some code for better look

--- SightLocator/src/com/iolab/sightlocator/SightsMapFragment.java
+++ SightLocator/src/com/iolab/sightlocator/SightsMapFragment.java
@@ -70,8 +70,8 @@ public class SightsMapFragment extends Fragment {
 							.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
 			
 			gMap.addMarker(new MarkerOptions()
-		    		.position(new LatLng(49.8367019,24.0048451))
-		    		.title("Hello world"));
+					.position(new LatLng(49.8367019,24.0048451))
+					.title("Hello world"));
 
 		}
 	}
