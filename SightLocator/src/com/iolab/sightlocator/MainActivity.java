b6f8d41dc35cd7612b9a45dc6286ea79971dbfe4
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2014-12-03 15:59:53 +0200

    Added "permanent location" functionality

--- SightLocator/src/com/iolab/sightlocator/MainActivity.java
+++ SightLocator/src/com/iolab/sightlocator/MainActivity.java
@@ -1,8 +1,7 @@
 package com.iolab.sightlocator;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
