55cde432215c30fd1bd841971d3c7cc96660bf62
Taras Tymchyshyn <taras.tymchyshyn@gmail.com>
2014-12-16 13:26:49 +0200

    formatted code

--- SightLocator/src/com/iolab/sightlocator/TouchEventListenerFrameLayout.java
+++ SightLocator/src/com/iolab/sightlocator/TouchEventListenerFrameLayout.java
@@ -5,27 +5,27 @@ import android.view.MotionEvent;
 import android.widget.FrameLayout;
 
 public class TouchEventListenerFrameLayout extends FrameLayout {
-	
+
 	boolean mMapIsTouched = false;
 
 	public TouchEventListenerFrameLayout(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
-	public boolean dispatchTouchEvent(MotionEvent ev){
+	public boolean dispatchTouchEvent(MotionEvent ev) {
 		switch (ev.getAction()) {
-        case MotionEvent.ACTION_DOWN:
-        mMapIsTouched = true;
-        break;
+		case MotionEvent.ACTION_DOWN:
+			mMapIsTouched = true;
+			break;
 
-    case MotionEvent.ACTION_UP:
-        mMapIsTouched = false;
-        break;
-        }
+		case MotionEvent.ACTION_UP:
+			mMapIsTouched = false;
+			break;
+		}
 
-        return super.dispatchTouchEvent(ev);
+		return super.dispatchTouchEvent(ev);
 	}
 
 }
