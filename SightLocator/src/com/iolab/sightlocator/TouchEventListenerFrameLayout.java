4d0fc9d5cd6ab3defd80c894481868f889e183b8
oles2008 <oles2008@i.ua>
2014-12-16 13:14:58 +0200

    Forgot to add TouchEventListenerframelayout

---
+++ SightLocator/src/com/iolab/sightlocator/TouchEventListenerFrameLayout.java
@@ -0,0 +1,32 @@
+package com.iolab.sightlocator;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.FrameLayout;
+
+public class TouchEventListenerFrameLayout extends FrameLayout {
+	
+	boolean mMapIsTouched = false;
+
+	public TouchEventListenerFrameLayout(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev){
+		switch (ev.getAction()) {
+        case MotionEvent.ACTION_DOWN:
+        mMapIsTouched = true;
+        break;
+
+    case MotionEvent.ACTION_UP:
+        mMapIsTouched = false;
+        break;
+        }
+
+        return super.dispatchTouchEvent(ev);
+	}
+
+}
