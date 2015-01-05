f1ebde826548ed1b9cbe1f754e275484db763ce4
SummerLake <bigame15@gmail.com>
2015-01-05 19:04:44 +0200

    new tags were added

--- SightLocator/src/com/iolab/sightlocator/Tags.java
+++ SightLocator/src/com/iolab/sightlocator/Tags.java
@@ -3,4 +3,6 @@ package com.iolab.sightlocator;
 public class Tags {
 
 	public final static String MARKERS = "markers";
+	public final static String SIGHT_DESCRIPTION = "sight_description";
+	public final static String PATH_TO_IMAGE = "path_to_image";
 }
