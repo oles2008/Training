3f8ed3d747d11012e0530f34802493c93b262fe8
oles2008 <oles2008@i.ua>
2015-08-26 20:36:16 +0300

    CategoryUtils added

---
+++ SightLocator/src/com/iolab/sightlocator/CategoryUtils.java
@@ -0,0 +1,14 @@
+package com.iolab.sightlocator;
+
+public class CategoryUtils {
+	
+	private CategoryUtils(){}
+
+	public static int getCategoryMarkerResId(String category) {
+		return 0;
+	}
+	
+	public static int getCategorySelectedMarkerResId(String category) {
+		return 0;
+	}
+}
