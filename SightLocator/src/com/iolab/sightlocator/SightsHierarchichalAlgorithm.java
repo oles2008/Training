2a3908b19a3b68eb8ba8a97d9bf66f7e8f23d168
ukrtravel <oles2008@i.ua>
2015-09-12 04:17:05 +0300

    Fixed clustering (another approximation:))

--- SightLocator/src/com/iolab/sightlocator/SightsHierarchichalAlgorithm.java
+++ SightLocator/src/com/iolab/sightlocator/SightsHierarchichalAlgorithm.java
@@ -1,12 +1,9 @@
 package com.iolab.sightlocator;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.Cluster;
@@ -28,83 +25,22 @@ public class SightsHierarchichalAlgorithm extends
 		Set<? extends Cluster<SightMarkerItem>> clustersBiggerZoom = super.getClusters(zoom+1);
 		Set<Cluster<SightMarkerItem>> resultSet = new HashSet<Cluster<SightMarkerItem>>();
 		for(Cluster<SightMarkerItem> cluster: clusterSet){
-			if(shouldClusterBeSplit(cluster, clustersBiggerZoom)){
-				Log.d("MyLogs", "cluster "+printCluster(cluster)+" split");
-				resultSet.addAll(splitClusterAccordingToHierarchy(cluster));
-			}else{
-				Log.d("MyLogs", "cluster "+printCluster(cluster)+" not split");
+			Set<Cluster<SightMarkerItem>> splitClusters = splitClusterAccordingToHierarchy(cluster);
+			
+			if(isBiggerZoomClusteringFiner(splitClusters, clustersBiggerZoom)){
+				resultSet.addAll(splitClusters);
+			} else {
 				resultSet.add(cluster);
 			}
 		}
 		return resultSet;
-//		boolean isTheSame = true;
-//		for (Cluster<SightMarkerItem> cluster : clusterSet) {
-//			for (Cluster<SightMarkerItem> clusterFromBiggerZoom : clusterSetBiggerZoom) {
-//				Set<SightMarkerItem> intersection = new HashSet<SightMarkerItem>(
-//						clusterFromBiggerZoom.getItems());
-//				intersection.retainAll(cluster.getItems());
-//				if (!intersection.isEmpty()
-//						&& !clusterFromBiggerZoom.getItems().containsAll(
-//								cluster.getItems())) {
-//					isTheSame = false;
-//				}
-//			}
-//		}
-//		Log.d("MyLogs", "isTheSame: "+isTheSame);
-//		if (isTheSame) {
-//			return clusterSet;
-//		}
-//		//the end
-//		
-//		List<Cluster<SightMarkerItem>> initialClusterList = new ArrayList<Cluster<SightMarkerItem>>(clusterSet);
-//		clusterSet.clear();
-//		for(Cluster<SightMarkerItem> cluster: initialClusterList){
-//			List<int[]> parentIDsArrays = new ArrayList<int[]>();
-//			List<SightMarkerItem> clusterItems = new ArrayList<SightMarkerItem>(cluster.getItems());
-//			for(int i=0;i<clusterItems.size();i++){
-//				SightMarkerItem item = clusterItems.get(i);
-//				parentIDsArrays.add(item.getParentIDs());
-//			}
-//			List<ClusterGroup> clusterGroups = ItemGroupAnalyzer.split(parentIDsArrays, 3);
-//			for(ClusterGroup clusterGroup: clusterGroups){
-//				Set<SightMarkerItem> items = new HashSet<SightMarkerItem>();
-//				double averageLat=0;
-//				double averageLong=0;
-//				for(int index: clusterGroup.getListOfElements()){
-//					SightMarkerItem item = clusterItems.get(index); 
-//					items.add(item);
-//					averageLat += item.getLatitude();
-//					averageLong += item.getLongitude();
-//				}
-//				if(items.size()==0){
-//					continue;
-//				}
-//				averageLat /= items.size();
-//				averageLong /= items.size();
-//				StaticCluster<SightMarkerItem> newCluster = new StaticCluster<SightMarkerItem>( new LatLng(averageLat, averageLong));
-//				for(SightMarkerItem item: items){
-//					newCluster.add(item);
-//				}
-//				//TODO use the newCluster's common parent
-//				clusterSet.add(newCluster);
-//			}
-//		}
-//		return clusterSet;
 	}
+
 	
 	/* **************************************************************************** */
     /* ******************************** Utility API ******************************* */
     /* **************************************************************************** */
 
-	private boolean shouldClusterBeSplit(Cluster<SightMarkerItem> cluster,
-			Set<? extends Cluster<SightMarkerItem>> clustersBiggerZoom) {
-		for (Cluster<SightMarkerItem> clusterBiggerZoom : clustersBiggerZoom) {
-			if (clusterBiggerZoom.getItems().containsAll(cluster.getItems())) {
-				return false;
-			}
-		}
-		return true;
-	}
 
 	private Set<Cluster<SightMarkerItem>> splitClusterAccordingToHierarchy(
 			Cluster<SightMarkerItem> initialCluster) {
@@ -146,6 +82,28 @@ public class SightsHierarchichalAlgorithm extends
 		return clusterSet;
 	}
 	
+	/**
+	 * Determines whether the clustering under bigger zoom is finer than the given split cluster.
+	 * 
+	 * @param splitClusters the clusters that appeared after splitting the original one
+	 * @param clustersBiggerZoom the set of clusters obtained by the distance-based algorithm at a bigger zoom
+	 */
+	private boolean isBiggerZoomClusteringFiner(Set<Cluster<SightMarkerItem>> splitClusters, Set<? extends Cluster<SightMarkerItem>> clustersBiggerZoom){
+		for(Cluster<SightMarkerItem> clusterBiggerZoom: clustersBiggerZoom){
+			for(Cluster<SightMarkerItem> splitCluster: splitClusters){
+				if(splitCluster.getItems().containsAll(clusterBiggerZoom.getItems())){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Used for debugging.
+	 * 
+	 * @param the {@link Cluster} to be printed.
+	 */
 	private String printCluster(Cluster<SightMarkerItem> cluster){
 		if(cluster == null){
 			return null;
