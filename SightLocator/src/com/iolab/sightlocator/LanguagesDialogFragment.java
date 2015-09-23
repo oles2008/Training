aa870c48f37a05c971bf890ac67bbff7d0d23aa6
SummerLake <bigame15@gmail.com>
2015-09-23 19:12:41 +0300

    draft for changing language dialog

--- SightLocator/src/com/iolab/sightlocator/LanguagesDialogFragment.java
+++ SightLocator/src/com/iolab/sightlocator/LanguagesDialogFragment.java
@@ -1,5 +1,8 @@
 package com.iolab.sightlocator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -8,12 +11,14 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
 
 public class LanguagesDialogFragment extends DialogFragment{
-	int selectedItem = -1;
+	int mSelectedItem = -1;
+	String mStr="";
 	public interface LanguagesDialogListener {
 		public void onLanguagesDialogPositiveClick(DialogFragment dialog);
 		public void onLanguagesDialogNegativeClick(DialogFragment dialog);
@@ -31,15 +36,37 @@ public class LanguagesDialogFragment extends DialogFragment{
 	}
 
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-	
+		List<String> itemAvailableLanguages = new ArrayList<String>();//lang
+		CharSequence[] arrayOfLanguages;//lang
+		Cursor cursor = Appl.sightsDatabaseOpenHelper.getReadableDatabase(). //lang
+				rawQuery( //lang
+		"SELECT CASE WHEN DESCRIPTION_EN IS NOT NULL AND DESCRIPTION_EN <> '' THEN 'en' ELSE NULL END,"+ //lang
+		" CASE WHEN DESCRIPTION_UK IS NOT NULL AND DESCRIPTION_EN <> '' THEN 'uk' ELSE NULL END"+ //lang
+				" FROM " + SightsDatabaseOpenHelper.TABLE_NAME + //lang
+				" WHERE " + SightsDatabaseOpenHelper.COLUMN_ID + " = 11", null);  //lang change hardcoded id to once from bandle
+		
+		if(cursor.getCount() == 1){  //lang
+			cursor.moveToFirst();
+			for(int i=0;i<cursor.getColumnCount();i++){
+				if (!cursor.isNull(i)){itemAvailableLanguages.add(cursor.getString(i));}
+			}  //lang
+		}  //lang
+		cursor.close();//lang
+		
+		if(itemAvailableLanguages.isEmpty()){
+			arrayOfLanguages = getResources().getStringArray(R.array.content_language);}//lang
+		else{
+			arrayOfLanguages = (CharSequence[]) itemAvailableLanguages.toArray(new String[itemAvailableLanguages.size()]);}//lang
+			//Log.w("ihor",itemAvailableLanguages.toArray(new String[itemAvailableLanguages.size()]).getClass().getName());}//lang
+		
 	    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-	    builder.setTitle(R.string.languages_dialog_title)
-	           .setSingleChoiceItems(R.array.apps_language,-1, new DialogInterface.OnClickListener() {
+	    builder.setTitle(R.string.content_lang_dialog_title)
+	    	   .setSingleChoiceItems(arrayOfLanguages,-1, new DialogInterface.OnClickListener() {
 	        	   
 	               public void onClick(DialogInterface dialog, int which) {
 	               // The 'which' argument contains the index position
 	               // of the selected item
-	            	   selectedItem = which;
+	            	   mSelectedItem = which;
 	           }
 	     
 	    });
@@ -47,13 +74,11 @@ public class LanguagesDialogFragment extends DialogFragment{
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				// Set into SharedPreferences variable selected in dialog language
-				if(selectedItem != -1){
+				if(mSelectedItem != -1){
 				SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
 				SharedPreferences.Editor editor = sharedPref.edit();
-				editor.putInt(getString(R.string.application_language), selectedItem);
-				editor.commit();
-				selectedItem = -1;
-				//Log.w("ihor",Integer.toString(sharedPref.getInt(getString(R.string.application_language),0)));
+				editor.putInt(getString(R.string.content_language), mSelectedItem);
+				editor.commit();				
 				}
 				mListener.onLanguagesDialogPositiveClick(LanguagesDialogFragment.this);
 			}
@@ -67,6 +92,7 @@ public class LanguagesDialogFragment extends DialogFragment{
 				dialog.cancel();
 			}
 		});
+	    
 	    return builder.create();
 	}
 }
