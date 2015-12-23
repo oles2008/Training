422fc650fcc1d35f4c9e7dc51c8d2c7ef74741d5
Mykhaylo <Mykhaylo@Mike-PC>
2015-12-23 22:28:22 +0200

    Added verification of gps module.

--- SightLocator/src/com/iolab/sightlocator/MainActivity.java
+++ SightLocator/src/com/iolab/sightlocator/MainActivity.java
@@ -1,9 +1,18 @@
 package com.iolab.sightlocator;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -18,10 +27,11 @@ public class MainActivity extends BaseActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Context context = getBaseContext();
 		
 		// checking if GooglePlayServices is installed
 		int status = GooglePlayServicesUtil
-				.isGooglePlayServicesAvailable(getBaseContext());
+				.isGooglePlayServicesAvailable(context);
 
 		// Showing status
 		if (status != ConnectionResult.SUCCESS) { // Google Play Services are not available
@@ -50,8 +60,47 @@ public class MainActivity extends BaseActivity
             getTextView().setTextSize(14);
             getFragmentManager().beginTransaction().show(mapFragment).addToBackStack(null).commit();
         }
+                
+        // check if gps module is presented, switched off
+        LocationManager mgr = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+        if (mgr != null) {
+        	if (mgr.getAllProviders().contains(LocationManager.GPS_PROVIDER)) {
+        		if(mgr.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+					Toast.makeText(Appl.appContext, "GPS is enabled", 
+        					Toast.LENGTH_SHORT).show();
+        		} else {
+        			showGPSDisabledAlertToUser();
+        		}
+        	}
+        	else {
+        		Toast.makeText(getApplicationContext(), "GPS is not found", 
+        					Toast.LENGTH_SHORT).show();
+        	}
+        }
     }
 
+    private void showGPSDisabledAlertToUser(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS is disabled in your device. Would you like to enable it?")
+        .setCancelable(false)
+        .setPositiveButton("Goto Settings Page To Enable GPS",
+                new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id){
+                Intent callGPSSettingIntent = new Intent(
+                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(callGPSSettingIntent);
+            }
+        });
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id){
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+    
     private TextView getTextView() {
         Fragment textFragment = getFragmentManager()
                 .findFragmentById(R.id.text_fragment);
