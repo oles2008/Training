abfd863449d563532e064b0452117986028bc4e7
Mykhaylo <Mykhaylo@Mike-PC>
2016-03-26 09:35:29 +0000

    Implemented analysis of item group parent ID algorithm

--- SightLocator/src/com/iolab/sightlocator/ItemViewAnalyzerOptimized.java
+++ SightLocator/src/com/iolab/sightlocator/ItemViewAnalyzerOptimized.java
@@ -6,8 +6,11 @@ import java.util.Map;
 
 public class ItemViewAnalyzerOptimized {
 
-	public static int findCommonParent(List<int[]> parentIDsList, int percentageToIgnore) {
+	public static int findCommonParent(List<int[]> parentIDsList, int ignoreLevelPercentage) {
+		int analysisId = -1;
 		int numberOfItems = parentIDsList.size();
+		
+		// general hierarchy analysis. NOT FINISHED
 		Map<Integer, Integer> parentOccurrenceMap = new HashMap<Integer, Integer>();
 		for(int[] parentIDs: parentIDsList) {
 			for(int parentID: parentIDs) {
@@ -15,6 +18,45 @@ public class ItemViewAnalyzerOptimized {
 			}
 		}
 		
+		// hierarchy level based algorithm
+		// no optimization of proper level start yet
+		boolean continueCond = true;
+		int level = 1;
+		while(continueCond){
+			// build occurrence map for level = "level" (starting from highest)
+			for(int[] parentIDs: parentIDsList) {
+				// check if hierarchy is deep enough and add parent to accurrence map
+				if(parentIDs.length >= level){
+					addOccurence(parentIDs[parentIDs.length - level], parentOccurrenceMap);
+				}
+			}
+			
+			// get the parentID which passes ignore level with smallest overhead
+			double overhead = 100;
+			int levelOptId = -1;
+			for(int parentId : parentOccurrenceMap.keySet()){
+				// get frequency of parentId
+				double freq = parentOccurrenceMap.get(parentId) / (double)numberOfItems * 100;
+				
+				// check if freq passes level and has smaller overhead
+				if(freq > ignoreLevelPercentage && freq - ignoreLevelPercentage < overhead){
+					overhead = freq - ignoreLevelPercentage;
+					levelOptId = parentId;
+				}
+			}
+			
+			// setup analysis id
+			if(levelOptId != -1){
+				analysisId = levelOptId;
+			} else {
+				continueCond = false;
+			}
+			
+			// go to the next level
+			level++;
+		}
+		
+		return analysisId;
 	}
 	
 	private static void addOccurence(int parentID, Map<Integer, Integer> occurenceMap) {
