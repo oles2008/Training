76b7e11e02d764e2a12247de1bf32b88f2e15b30
ukrtravel <oles2008@i.ua>
2016-03-31 01:42:32 +0300

    Finishing another version of the method, findCommonParentId2().

--- SightLocator/src/com/iolab/sightlocator/ItemViewAnalyzerOptimized.java
+++ SightLocator/src/com/iolab/sightlocator/ItemViewAnalyzerOptimized.java
@@ -12,11 +12,11 @@ public class ItemViewAnalyzerOptimized {
 		
 		// general hierarchy analysis. NOT FINISHED
 		Map<Integer, Integer> parentOccurrenceMap = new HashMap<Integer, Integer>();
-		for(int[] parentIDs: parentIDsList) {
-			for(int parentID: parentIDs) {
-				addOccurence(parentID, parentOccurrenceMap);
-			}
-		}
+//		for(int[] parentIDs: parentIDsList) {
+//			for(int parentID: parentIDs) {
+//				addOccurence(parentID, parentOccurrenceMap);
+//			}
+//		}
 		
 		// hierarchy level based algorithm
 		// no optimization of proper level start yet
@@ -59,6 +59,41 @@ public class ItemViewAnalyzerOptimized {
 		return analysisId;
 	}
 	
+	public static int findCommonParent2(List<int[]> parentIDsList, int ignoreLevelPercentage) {
+		int commonParentId = -1;
+		for(int level=0;;level++) {			
+			int mostCommonItemFromLevel = getMostCommonItemFromLevel(parentIDsList, ignoreLevelPercentage, level);
+			if(mostCommonItemFromLevel != -1) {
+				commonParentId = mostCommonItemFromLevel;
+			} else {
+				break;
+			}
+		}
+		return commonParentId;
+	}
+	
+	private static int getMostCommonItemFromLevel(List<int[]> parentIDsList, int level, int percentageToIgnore) {
+		Map<Integer, Integer> parentOccurrenceMap = new HashMap<Integer, Integer>();
+		for(int[] parentIDs: parentIDsList) {
+			// check if hierarchy is deep enough and add parent to occurrence map
+			if(parentIDs.length >= level){
+				addOccurence(parentIDs[level], parentOccurrenceMap);
+			}
+		}
+		int mostCommonId = getKeyByMaxValue(parentOccurrenceMap);
+		if(mostCommonId == -1) {
+			return -1;
+		}
+		double freq = parentOccurrenceMap.get(mostCommonId) / (double)parentIDsList.size() * 100;
+		
+		// check if freq passes level
+		if(freq > 100-percentageToIgnore){
+			return mostCommonId;
+		} else {
+			return -1;
+		}
+	}
+	
 	private static void addOccurence(int parentID, Map<Integer, Integer> occurenceMap) {
 		if(!occurenceMap.containsKey(parentID)) {
 			occurenceMap.put(parentID, 0);
@@ -66,5 +101,17 @@ public class ItemViewAnalyzerOptimized {
 			int currentValue = occurenceMap.get(parentID);
 			occurenceMap.put(parentID, ++currentValue);
 		}
+	}
+	
+	private static int getKeyByMaxValue(Map<Integer, Integer> parentOccurrenceMap) {
+		int maxKey = -1;
+		int maxValue = 0;
+		for(int key: parentOccurrenceMap.keySet()) {
+			if(parentOccurrenceMap.get(key) > maxValue) {
+				maxKey = key;
+				maxValue = parentOccurrenceMap.get(key);
+			}
+		}
+		return maxKey;
 	}
 }
